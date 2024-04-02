afce595f72cf1977d3dab3519d16038b28b24703
Kalina0502 <kkj.04b14@gmail.com>
2024-04-02 19:08:01 +0300

    Models Added

---
+++ PetWorld.Infrastructure/Data/Models/GroomingReservation.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PetWorld.Infrastructure.Data.Models
+{
+    public class GroomingReservation
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Comment("Pet")]
+        public int PetId { get; set; }
+
+        [ForeignKey(nameof(PetId))]
+        public Pet Pet { get; set; } = null!;
+
+        [Required]
+        [Comment("Check-in date")]
+        public DateTime StartTime { get; set; }
+
+        [Required]
+        [Comment("Check-out date")]
+        public DateTime EndTime { get; set; }
+
+        [Required]
+        public int GroomerId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(GroomerId))]
+        public Groomer Groomer { get; set; } = null!;
+
+        [Required]
+        public int GroomingTypeId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(GroomingTypeId))]
+        public GroomingType GroomingType { get; set; } = null!;
+    }
+}
---
+++ PetWorld.Infrastructure/Data/Models/GroomingType.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PetWorld.Infrastructure.Data.Models
+{
+    [Comment("Service Type description")]
+    public class GroomingType
+    {
+        [Key]
+        [Comment("ServiceType identifier")]
+        public int Id { get; set; }
+
+        [Required]
+        [Comment("Name of the service")]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [Comment("Agent identifier")]
+        public int AgentId { get; set; }
+
+        [ForeignKey(nameof(AgentId))]
+        public Agent Agent { get; set; } = null!;
+    }
+}
