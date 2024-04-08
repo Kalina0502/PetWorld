77c2bf0b3ddbb97ec6810c1447169655b4e340b2
Kalina0502 <kkj.04b14@gmail.com>
2024-04-08 18:30:18 +0300

    Namespace Corrected

--- PetWorld.Core/Constants/MessageConstants.cs
+++ PetWorld.Core/Constants/MessageConstants.cs
@@ -1,10 +1,12 @@
 ﻿namespace PetWorld.Core.Constants
 {
-    public static class MessageConstats
+    public static class MessageConstants
     {
         public const string RequiredMessage = "The {0 field is required}";
 
         public const string LengthMessage = "The field {0} must be between {1} and {2} characters long";
 
+        public const string PhoneExists = "Phone number alredy exists";
+        
     }
 }
