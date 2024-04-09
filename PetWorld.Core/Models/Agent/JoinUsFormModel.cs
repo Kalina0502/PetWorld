56d8dba526287d9a5b94c3b64436c60d9cd11cf7
Kalina0502 <kkj.04b14@gmail.com>
2024-04-09 16:15:24 +0300

    Agent Model renamed

--- PetWorld.Core/Models/Agent/JoinUsFormModel.cs
+++ PetWorld.Core/Models/Agent/JoinUsFormModel.cs
@@ -1,15 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using static PetWorld.Core.Constants.MessageConstats;
+using static PetWorld.Core.Constants.MessageConstants;
 using static PetWorld.Infrastructure.Constants.DataConstants;
 
 namespace PetWorld.Core.Models.Agent
 {
-    public class BecomeAgentFormModel
+    public class JoinUsFormModel
     {
         [Required(ErrorMessage = RequiredMessage)]
-        [StringLength(PhoneNumberLength,
+        [StringLength(PhoneNumberMaxLength,
             MinimumLength = PhoneNumberMinLength,
-            ErrorMessage =LengthMessage)]
+            ErrorMessage = LengthMessage)]
         [Display(Name = "Phone number")]
         [Phone]
         public string PhoneNumber { get; set; } = null!;
