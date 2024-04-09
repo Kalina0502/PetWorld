a3eee5b224f441f0181c3b8676da510f1998d726
Kalina0502 <kkj.04b14@gmail.com>
2024-04-09 16:14:38 +0300

    Agent Attribute moified/added

---
+++ PetWorld/Attributes/MustBeAgentAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using PetWorld.Core.Contracts;
+using System.Security.Claims;
+using PetWorld.Controllers;
+
+namespace PetWorld.Attributes
+{
+    public class MustBeAgentAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            IAgentService? agentService = context.HttpContext.RequestServices.GetService<IAgentService>();
+
+            if (agentService == null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            if (agentService != null
+                && agentService.ExistsByIdAsync(context.HttpContext.User.Id()).Result == false)
+            {
+                context.Result = new RedirectToActionResult(nameof(AgentController.JoinUs), "Agent", null);
+            }
+        }
+    }
+}
