5b6b3e2547b5087767046bacd2ecb12c6e893456
Kalina0502 <kkj.04b14@gmail.com>
2024-04-22 00:33:53 +0300

    HotelService Added

---
+++ PetWorld.Core/Contracts/IHotelService.cs
@@ -0,0 +1,24 @@
+﻿using PetWorld.Core.Enumerations;
+using PetWorld.Core.Models.Hotel;
+using PetWorld.Infrastructure.Data.Models;
+
+
+namespace PetWorld.Core.Contracts
+{
+    public interface IHotelService
+    {
+      //  Task<Room?> FindFirstAvailableRoomAsync(string roomType, DateTime? checkInDate, DateTime? checkOutDate);
+
+      //  Task<IEnumerable<string>> AllRoomTypesAsync();
+
+        Task<IEnumerable<string>> AllRoomTypeNamesAsync();
+
+        Task<HotelRoomsQueryServiceModel> AllAsync(
+            string? roomType = null,
+            DateTime? checkInDate = null,
+            DateTime? checkOutDate = null,
+            HotelSorting sorting = HotelSorting.Newest,
+            int currentPage = 1,
+            int roomsPerPage = 1);
+    }
+}
---
+++ PetWorld.Core/Services/HotelService.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetWorld.Core.Contracts;
+using PetWorld.Core.Enumerations;
+using PetWorld.Core.Models.Hotel;
+using PetWorld.Infrastructure.Common;
+using PetWorld.Infrastructure.Data.Models;
+using System.Reflection.Metadata;
+
+namespace PetWorld.Core.Services
+{
+    public class HotelService : IHotelService
+    {
+        private readonly IRepository repository;
+
+        public HotelService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        public async Task<IEnumerable<RoomTypeServiceModel>> AllRoomTypesAsync()
+        {
+            return await repository.AllReadOnly<RoomType>()
+                 .Select(c => new RoomTypeServiceModel()
+                 {
+                     Id = c.Id,
+                     Name = c.Name,
+                 })
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<string>> AllRoomTypeNamesAsync()
+        {
+            return await repository.AllReadOnly<RoomType>()
+                .Select(rt => rt.Name)
+                .Distinct()
+                .ToListAsync();
+        }
+
+        public async Task<HotelRoomsQueryServiceModel> AllAsync(
+            string? roomType = null,
+            DateTime? checkInDate = null, 
+            DateTime? checkOutDate = null, 
+            HotelSorting sorting = HotelSorting.Newest, 
+            int currentPage = 1, 
+            int roomsPerPage = 1)
+        {
+            checkInDate ??= DateTime.Today;
+            checkOutDate ??= DateTime.Today;
+
+            var hotelRoomsToShow = repository.AllReadOnly<Room>();
+
+            if (roomType != null)
+            {
+                hotelRoomsToShow = hotelRoomsToShow
+                    .Where(hr => hr.RoomType.Name == roomType);
+            }
+
+            if (checkInDate.HasValue && checkOutDate.HasValue)
+            {
+                hotelRoomsToShow = hotelRoomsToShow
+                    .Where(hr => !repository.AllReadOnly<RoomReservation>()
+                        .Any(r => r.RoomId == hr.Id &&
+                                  (r.CheckInDate <= checkOutDate.Value) && (r.CheckOutDate >= checkInDate.Value)));
+            }
+
+            var hotelRooms = await hotelRoomsToShow
+                .Skip((currentPage - 1) * roomsPerPage)
+                .Take(roomsPerPage)
+                .Select(hr => new HotelRoomServiceModel
+                {
+                    Id = hr.Id,
+                    RoomTypeId = hr.RoomTypeId,
+                    IsAvailable = hr.IsAvailable
+                })
+                .ToListAsync();
+
+            int totalRoomsCount = await hotelRoomsToShow.CountAsync();
+
+            return new HotelRoomsQueryServiceModel
+            {
+                TotalRoomsCount = totalRoomsCount,
+                HotelRooms = hotelRooms
+            };
+        }
+    }
+}
