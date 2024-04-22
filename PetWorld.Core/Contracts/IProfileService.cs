5e823a8f4d19e192e7c2a1d038c8b85573267e26
Kalina0502 <kkj.04b14@gmail.com>
2024-04-22 21:21:28 +0300

    ProfileService Added

---
+++ PetWorld.Core/Contracts/IProfileService.cs
@@ -0,0 +1,11 @@
+﻿using PetWorld.Core.Models;
+using PetWorld.Core.Models.Profile;
+
+namespace PetWorld.Core.Contracts
+{
+    public interface IProfileService
+    {
+        Task<int> CreateAsync(ProfileIndexViewModel model, string userId);
+
+    }
+}
---
+++ PetWorld.Core/Services/ProfileService.cs
@@ -0,0 +1,49 @@
+﻿using PetWorld.Core.Contracts;
+using PetWorld.Core.Models;
+using PetWorld.Core.Models.Adoption;
+using PetWorld.Core.Models.Profile;
+using PetWorld.Infrastructure.Common;
+using PetWorld.Infrastructure.Data.Models;
+
+namespace PetWorld.Core.Services
+{
+    public class ProfileService : IProfileService
+    {
+        private readonly IRepository repository;
+
+        private readonly IPetOwnerService petOwnerService;
+        private readonly IAdoptionService adoptionService;
+        private readonly IHotelService hotelService;
+
+        public ProfileService(IRepository _repository,
+            IPetOwnerService _petOwnerService,
+            IAdoptionService _adoptionService,
+            IHotelService _hotelService)
+        {
+            repository = _repository;
+            petOwnerService = _petOwnerService;
+            adoptionService = _adoptionService;
+            hotelService = _hotelService;
+        }
+
+        public async Task<int> CreateAsync(ProfileIndexViewModel model, string userId)
+        {
+            PetOwner petOwner = new PetOwner()
+            { 
+              PetOwnerFirstName = model.FirstName,
+              PetOwnerLastName = model.LastName,
+              Age = model.Age,
+              GenderId = model.GenderId,
+              PhoneNumber = model.PhoneNumber,
+              Email = model.Email,
+              UserId = userId
+            };
+
+            await repository.AddAsync(petOwner);
+            await repository.SaveChangesAsync();
+
+            return petOwner.Id;
+        }
+
+    }
+}
