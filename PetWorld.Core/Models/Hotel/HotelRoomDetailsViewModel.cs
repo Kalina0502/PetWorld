f77db0e6127e51575124f75382156debea557229
Kalina0502 <kkj.04b14@gmail.com>
2024-04-23 20:37:59 +0300

    HotelRoomDetailsViewModel deleted

--- PetWorld.Core/Models/Hotel/HotelRoomDetailsViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PetWorld.Core.Models.Hotel
-{
-    public class HotelRoomDetailsViewModel
-    {
-        public int Id { get; set; }
-
-        public string RoomType { get; set; } = null!;
-    }
-}
