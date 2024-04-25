03d4856f92c10b42e5123fe625db78f2b239778f
Kalina0502 <kkj.04b14@gmail.com>
2024-04-25 12:37:37 +0300

    Code optimized

--- PetWorld.Core/Models/Adoption/AdoptionDetailsViewModel.cs
+++ PetWorld.Core/Models/Adoption/AdoptionDetailsViewModel.cs
@@ -5,11 +5,6 @@ namespace PetWorld.Core.Models.Adoption
     public class AdoptionDetailsViewModel : IAdoptionModel
     {
         public int Id { get; set; }
-
-        public string Name { get; set; } = string.Empty;
-
-        public string City { get; set; } = string.Empty;
-
         public string ImageUrl { get; set; } = string.Empty;
     }
 }
